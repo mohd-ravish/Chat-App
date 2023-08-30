4152ec28076b5febbffaccd289dda399b19fe44f
mohd-ravish <mohdravish2003@gmail.com>
2023-08-30 22:59:12 +0530

    App.js Updated

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import io from "socket.io-client";
 import Chat from "./Chat";
 
-const socket = io.connect("http://localhost:4500");
+// const socket = io.connect("http://localhost:4500");
+const socket = io.connect("https://chat-verse-server.vercel.app/");
 
 function App() {
 
